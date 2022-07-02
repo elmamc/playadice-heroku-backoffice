acebd9e227cf4be1ad5bea0cd080c6067b1060c2
elmamc <antonio.marottoli@gmail.com>
2022-07-02 13:19:24 +0200

    prova

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const webAppName = 'playadice-heroku-backoffice';
 
 
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        `/dist/${webAppName}/index.html`));
 });
 
 app.listen(process.env.PORT || 5000, () => {
